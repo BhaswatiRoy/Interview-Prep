2b610ce31e5916dd2e17fa14610c268b6404e390
Bhaswati Roy <roybhaswati24@gmail.com>
2024-11-24 15:56:08 +0000

    Update Binary Tree Vertical Order Traversal.cpp

--- Binary Tree Vertical Order Traversal.cpp
+++ Binary Tree Vertical Order Traversal.cpp
@@ -1,4 +1,15 @@
-Problem: https://leetcode.com/problems/binary-tree-vertical-order-traversal
+Problem:
+Part 1: https://leetcode.com/problems/binary-tree-vertical-order-traversal/description/
+Part 2: https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/description
+
+For part 1 there are no additional conditions
+
+For part 2 the traversal requires sorting by vertical order first, 
+then by the row level (or horizontal distance), 
+and finally by value for nodes that share the same row and vertical position.
+
+For part 1, we don't need to store horizontal-level data, but for part 2, we need to because later, 
+we will sort based on horizontal-level data.
 
 /*
         1 (0,0)
@@ -8,53 +19,69 @@ Problem: https://leetcode.com/problems/binary-tree-vertical-order-traversal
 4 (2,-2)    5 (2,0)
 storing (horizontal level, vertical level)
 
-Approach:
-1. Store as {nodes, horizontal level, vertical level} in the queue
-2. horizontal + 1 for all children
-3. vertical + 1 for right children & vertical - 1 for left children
-4. store nodes based on the vertical level in a map
-5. then finally convert the map to a vector
+1. Do level order traversal and store {node, {horizontal, vertical}} in queue
+2. In map store key = vertical & value = {horizontal, node->val}
+3. Do level order traversal and store all nodes on the map based on their vertical position
+4. Now for each vertical position, the map has nodes stored as {horizontal, node->val}
+5. So if we sort it, then it will be sorted as a horizontal position the node value
 
-TC: O(n) -> BFS traversal 
-SC: O(n) -> queue datastructure used
+TC: O(n) + O(nlogn) = O(nlogn) -> BFS traversal + sorting in the worst case of a skewed tree where all nodes are in 1 vertical
+SC: O(n) + O(n) = O(n) -> queue + map
 */
 
-vector<vector<int>> verticalOrder(TreeNode* root) 
+vector<vector<int>> bfs(TreeNode *root)
 {
         if(root==NULL)
         {
             return {};
         }
-        //{node, {horizontal point, vertical point}}
         queue<pair<TreeNode*,pair<int,int>>>q;
+        // Store {vertical -> {horizontal, node->val}}
+        map<int,vector<pair<int,int>>>mp;
+        //{node,{horizontal,vertical}}
         q.push({root,{0,0}});
-        map<int,vector<int>>mp;
-        vector<vector<int>>ans;
         while(!q.empty())
         {
-            auto temp=q.front();
-            TreeNode* node = temp.first;
-            int horizontal = temp.second.first;
-            int vertical = temp.second.second;
-            //store nodes according to their vertical position in the map
-            mp[vertical].push_back(node->val);
-            q.pop();
-            //for left & right children update the horizontal & vertical indices properly
-            if(node->left!=NULL)
+            int size=q.size();
+            //for every horizontal level store in a map according to vertical level
+            for(int i=0;i<size;i++)
             {
-                //horizontal+1, vertical-1
-                q.push({node->left,{horizontal+1,vertical-1}});
-            }
-            if(node->right!=NULL)
-            {
-                //horizontal+1, vertical+1
-                q.push({node->right,{horizontal+1,vertical+1}});
+                auto temp=q.front();
+                TreeNode* node=temp.first;
+                int horizontal=temp.second.first;
+                int vertical=temp.second.second;
+                q.pop();
+                mp[vertical].push_back({horizontal, node->val});
+                //if children are not null then push them to the queue
+                if(node->left!=NULL)
+                {
+                    q.push({node->left,{horizontal+1, vertical-1}});
+                }
+                if(node->right!=NULL)
+                {
+                    q.push({node->right,{horizontal+1, vertical+1}});
+                }
             }
         }
-        //then finally store the nodes into a vector based on vertical order
+        vector<vector<int>>ans;
         for(auto it:mp)
         {
-            ans.push_back(it.second);
+            vector<pair<int,int>>temp=it.second;
+            //multiple nodes in same row,col needs to be in sorted format
+            //sorted by horizontal level first then node value
+            sort(temp.begin(),temp.end());
+            vector<int>v;
+            for(auto it:temp)
+            {
+                v.push_back(it.second);
+            }
+            ans.push_back(v);
         }
+        return ans;
+}
+
+vector<vector<int>> verticalTraversal(TreeNode* root) 
+{
+        vector<vector<int>>ans=bfs(root);
         return ans;
 }
