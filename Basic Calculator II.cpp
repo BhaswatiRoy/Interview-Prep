40978895fce40a7d2c85f1328a82bb649fdab333
Bhaswati Roy <roybhaswati24@gmail.com>
2024-11-28 15:49:01 +0000

    Create Basic Calculator II.cpp

---
+++ Basic Calculator II.cpp
@@ -0,0 +1,74 @@
+Problem: https://leetcode.com/problems/basic-calculator-ii/description
+
+/*
+Approach 1: stack approach
+1 + 2 * 3 - 5
+1. whenever we see numbers we store them in a variable "curr"
+2. when we see operators we see if its * or / then we pop out last number and do * or / with current
+3. if we see + then we directly add the number
+4. if we see - then we add -1 * number
+5. in end we pop out all stack elements and add them to our result
+
+TC: O(n) -> one pass
+SC: O(n) -> stack
+
+Approach 2: algebraic approach
+watch ayushi sharma yt video again
+
+TC: O(n)
+SC: O(1)
+*/
+
+int calculate(string s) 
+{
+        int n=s.length();
+        stack<int>st;
+        int curr=0;
+        char operation='+';
+        for(int i=0;i<n;i++)
+        {
+            //if digit
+            if(isdigit(s[i]))
+            {
+                curr = (curr*10) + (s[i]-'0');
+            }
+            //if not digit & not whitespace then operator or its the last character
+            if(s[i]=='+' || s[i]=='-' || s[i]=='*' || s[i]=='/' || i==n-1)
+            {
+                //for + directly push the number to stack
+                if(operation=='+')
+                {
+                    st.push(curr);
+                }
+                //for - push the negative of the number to stack
+                else if(operation=='-')
+                {
+                    st.push(curr*(-1));
+                }
+                //for * pop the last number then multiply with current then push to stack
+                else if(operation=='*')
+                {
+                    int top=st.top();
+                    st.pop();
+                    st.push(curr*top);
+                }
+                //for / pop the last number then multiply with current then push to stack
+                else if(operation=='/')
+                {
+                    int top=st.top();
+                    st.pop();
+                    st.push(top/curr);
+                }
+                operation=s[i];
+                curr=0;
+            }
+        }
+        //in end add everything in the stack to get the answer
+        int result=0;
+        while(!st.empty())
+        {
+            result+=st.top();
+            st.pop();
+        }
+        return result;
+}
