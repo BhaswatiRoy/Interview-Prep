46835f92bcf121355ed9b144d7e9e543f527c66f
Bhaswati Roy <roybhaswati24@gmail.com>
2024-12-11 16:50:56 +0000

    Update Valid Palindrome III.cpp

--- Valid Palindrome III.cpp
+++ Valid Palindrome III.cpp
@@ -1,7 +1,7 @@
 Problem: https://leetcode.com/problems/valid-palindrome-iii/description/
 
 /*
-Approach: gives MLE/WA for bigger strings
+Approach 1: gives MLE/WA for bigger strings
 1. Start from left = 0 & right = n-1
 2. If characters match then keep going
 3. If they dont match then remove left and check recursively for remaining string
@@ -12,6 +12,19 @@ Approach: gives MLE/WA for bigger strings
 
 TC: O(n*k) -> considering all n length cases & k removal cases
 SC: O(n*n) -> 2d array of n*n
+
+Approach 2: DP Memoization - Recursion stack overflow gives MLE
+1. Find the longest palindromic subsequence using LCS pattern
+2. LPS is basically LCS between a string & its reversed version
+3. string length - LPS length <= k -> max k deletions can lead to Palindrome
+
+TC: O(n^2) -> checking both strings of same length
+SC: O(n^2) -> 2d dp vector leading to MLE 
+
+Approach 3: DP Tabulation - Accepted
+1. Instead o recursion stack we iteratively fill the DP matrix
+TC: O(n^2) -> checking both strings of same length
+SC: O(n^2) -> 2d dp vector but does not lead to MLE as no recursion stack used
 */
 
 bool validpalindrome(int left, int right, int k, string s, vector<vector<int>>&dp)
@@ -51,4 +64,98 @@ bool isValidPalindrome(string s, int k)
         vector<vector<int>>dp(n,vector<int>(n,-1));
         bool ans=validpalindrome(left,right,k,s,dp);
         return ans;
+}
+
+//Approach 2: Memoization
+//find the longest palindromic subsequence length
+int lps(int n, int m, string s1, string s2,vector<vector<int>>&dp)
+{
+        //s1 = "abc", s2 = "" so lcs is "" of size 0
+        if(n==0 || m==0)
+        {
+            return 0;
+        }
+        if(dp[n][m]!=-1)
+        {
+            return dp[n][m];
+        }
+        //checking from last if characters from both strings have same value
+        //then reduce length of both strings in next recursion call
+        //1+ is done for counting this index value being same
+        if(s1[n-1]==s2[m-1])
+        {
+            return dp[n][m]=1+lps(n-1,m-1,s1,s2,dp);
+        }
+        //if not same then reduce length of each one by one in next recursion calls
+        //then return the max length obtained
+        return dp[n][m]=max(lps(n-1,m,s1,s2,dp),lps(n,m-1,s1,s2,dp));
+}
+bool isValidPalindrome(string s, int k) 
+{
+        string s1=s;
+        string s2=s;
+        reverse(s2.begin(),s2.end());
+        int n=s1.length(), m=s2.length();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        //LPS is basically LCS between string & reversed format
+        int lpslen=lps(n,m,s1,s2,dp);
+        //if we find LPS of length whose difference to original length is <= k
+        //then we can remove max k characters to get palindrome
+        if(n-lpslen<=k)
+        {
+            return true;
+        }
+        return false;
+}
+
+//Approach 3: Tabulation
+//find longest palindromic subsequence length
+int lps(int n, int m, string s1, string s2,vector<vector<int>>&dp)
+{
+        // Initialize base cases
+        //LCS with empty s2 is 0
+        for (int i = 0; i <= n; i++) 
+        {
+            dp[i][0] = 0; 
+        }
+        //LCS with empty s1 is 0
+        for (int j = 0; j <= m; j++) 
+        {
+            dp[0][j] = 0; 
+        }
+        //base case 0 id initialized so start iterating from 1
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else
+                {
+                    //check if we reduce i and we reduce j separately 
+                    //which of them is max
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+}
+bool isValidPalindrome(string s, int k) 
+{
+        string s1=s;
+        string s2=s;
+        reverse(s2.begin(),s2.end());
+        int n=s1.length(), m=s2.length();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        //LPS is basically LCS between string & reversed format
+        int lpslen=lps(n,m,s1,s2,dp);
+        //if we find LPS of length whose difference to original length is <= k
+        //then we can remove max k characters to get palindrome
+        if(n-lpslen<=k)
+        {
+            return true;
+        }
+        return false;
 }
